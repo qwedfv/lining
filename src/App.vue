5f755d4672b0581af9ccb9b56e84cdcab8faaf1c
李宁 <3058696700@qq.com>
2021-08-26 19:04:09 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!--  -->
+<template>
+  <div id="app">
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+</script>
+<style>
+html,body,#app{
+  height: 100%;
+}
+*{
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+
+      };
+    },
+    computed: {},
+    watch: {},
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+  }
+</script>
+<style>
+  
+</style>
