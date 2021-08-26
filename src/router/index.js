7b93a7c5afa1b715811403b6fc61c5b0769a6d3b
李宁 <3058696700@qq.com>
2021-08-26 23:39:52 +0800

    李宁第三天作业

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,10 @@ const routes = [
   {
     path: '*',
     redirect: '/'
+  },
+  {
+    path: '/pass',
+    component: () => import('@/views/pass')
   }
 ]
 
---
+++ src/views/pass.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        密码
+    </div>
+</template>
